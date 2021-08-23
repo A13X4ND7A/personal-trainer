c113a1e03941837bae28efeb2892bebba9290134
Alexandra Faller <alex@alexfaller.uk>
2021-08-23 21:07:29 +0100

    fixing layout.js again

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,7 @@
 import Footer from '../components/Footer/Footer';
 import Navbar from './Navbar/Navbar';
 
-export default function Layout({preview, children}) {
+export default function Layout({children}) {
 	return (
 		<div>
 			<Navbar />
